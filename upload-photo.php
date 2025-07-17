fc92c7947a1f3073145f0869ddb6055748cceef3
Marie-Sophie Duplouich <marie-sophieduplouich@macbookpro.home>
2025-07-17 22:08:35 +0200

    jumpscare demain et explode upload

--- upload-photo.php
+++ upload-photo.php
@@ -36,7 +36,7 @@
 
        <?php $photos_dir = opendir("photos");
         $file_name = readdir($photos_dir); // Premier fichier
-        echo $file_name . "test<br>";
+        echo $file_name;
         $file_name = readdir($photos_dir); // Deuxième fichier
         echo $file_name; ?>
 
